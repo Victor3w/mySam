a923fbf2763eb16b2767d55d6c980c3393f392c1
Victor_3w <1dodhe1@mail.ru>
2018-02-10 17:37:41 +0300

    Test push without login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,6 +65,9 @@
         </style>
     </head>
     <body>
+    <div class="test">
+        Hello!!!
+    </div>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
